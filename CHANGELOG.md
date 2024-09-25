0cea7a77632c2b593d41a07eaae1d15838c2feae
Dennis Enderink <dennis@1815.nl>
2024-09-25 14:10:15 +0200

    fix: better handling of image srcsets

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
 
+### Fixed
+
+- Better handling of image srcsets
+
 ## [0.7.0] - 2024-04-05
 
 ### Changed
--- src/Loading.php
+++ src/Loading.php
@@ -203,6 +203,14 @@ class Loading {
 		$filetype    = wp_check_filetype( $meta['file'] );
 
 		$sizes = [];
+		// original image size.
+		$sizes['original'] = [
+			'file'      => basename( $meta['file'] ),
+			'width'     => $meta['width'],
+			'height'    => $meta['height'],
+			'mime-type' => $filetype['type'],
+		];
+
 		foreach ( $image_sizes as $handle => $image_size ) {
 			$sizes[ $handle ] = [
 				'file'      => basename( $meta['file'] ),
@@ -239,14 +247,14 @@ class Loading {
 			foreach ( $meta['sizes'] as $size ) {
 				// check if width matches width source and if ratio is within 1px.
 				if ( $size['width'] === $source['value'] &&
-					wp_image_matches_ratio(
-						(int) $size['width'],
-						(int) $size['height'],
-						(int) $sizes[0],
-						(int) $sizes[1]
-					)
+				     wp_image_matches_ratio(
+					     (int) $size['width'],
+					     (int) $size['height'],
+					     (int) $sizes[0],
+					     (int) $sizes[1]
+				     )
 				) {
-					$source['url'] = $this->replace_url( $source['url'], $size );
+					$source['url'] = $this->replace_url( $this->get_attachment_url( $source['url'], $attachment_id ), $size );
 					continue 2;
 				}
 			}
